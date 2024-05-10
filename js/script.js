4cb494ccde925ea1e2ea91ba32b25da3546af978
Yoi96 <961761102@qq.com>
2024-05-10 12:25:22 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-/**
-* inspired by:
-* http://collectui.com/designers/prakhar/contact-us
-*/
+function submitForm() {
+        var firstName = document.getElementById("fname").value;
+        var lastName = document.getElementById("lname").value;
+        var email = document.getElementById("email").value;
+        var subject = document.getElementById("subject").value;
+
+        // Constructing the mailto URL with form data
+        var mailtoLink = "mailto:xiaoyiliu96xyl@gmail.com" +
+            "?subject=" + encodeURIComponent("Contact Form Submission") +
+            "&body=" + encodeURIComponent("First Name: " + firstName + "\n" +
+                "Last Name: " + lastName + "\n" +
+                "Email: " + email + "\n" +
+                "Message: " + subject);
+
+        // Opening the mail client with the mailto link
+        window.location.href = mailtoLink;
