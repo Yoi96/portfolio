2ccb61aa016ee62a7877776c50962c7199d19a47
Yoi96 <961761102@qq.com>
2024-05-16 11:37:54 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
-function submitForm() {
-        var firstName = document.getElementById("fname").value;
-        var lastName = document.getElementById("lname").value;
-        var email = document.getElementById("email").value;
-        var subject = document.getElementById("subject").value;
-
-        // Constructing the mailto URL with form data
-        var mailtoLink = "mailto:xiaoyiliu96xyl@gmail.com" +
-            "?subject=" + encodeURIComponent("Contact Form Submission") +
-            "&body=" + encodeURIComponent("First Name: " + firstName + "\n" +
-                "Last Name: " + lastName + "\n" +
-                "Email: " + email + "\n" +
-                "Message: " + subject);
-
-        // Opening the mail client with the mailto link
-        window.location.href = mailtoLink;
+function validateForm() {
+            var fname = document.forms["contactForm"]["firstname"].value;
+            var lname = document.forms["contactForm"]["lastname"].value;
+            var email = document.forms["contactForm"]["email"].value;
+            var subject = document.forms["contactForm"]["subject"].value;
+            
+            if (fname == "" || lname == "" || email == "" || subject == "") {
+                alert("Please fill in all fields");
+                return false;
+            }
+        }
