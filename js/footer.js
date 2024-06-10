62919ebc903b77ab26990327360b220148701f84
Yoi96 <961761102@qq.com>
2024-06-10 04:27:14 -0400

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var footerDiv = document.createElement("div");
+    footerDiv.id = "footer";
+    footerDiv.className = "footer";
+    footerDiv.innerHTML = 'Copyright &copy; Xiaoyi Liu 2024';
+    document.body.appendChild(footerDiv);
+};
